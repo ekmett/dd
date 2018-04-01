86b7049c0b6e917fa23e925edbc01fe42e361d16
Edward Kmett <ekmett@gmail.com>
2018-04-01 15:30:01 -0400

    verbatim works, -Wall

--- DD.hs
+++ DD.hs
@@ -7,5 +7,9 @@ import qualified Language.C.Inline.Unsafe as CU ()
 C.context C.cppCtx
 C.include "<iostream>"
 
+C.verbatim "void foo() { std::cout << \"Goodbye Cruel World\\n\"; }"
+
 main :: IO ()
-main = [C.block|void{ std::cout << "Hello, World!\n"; }|]
+main = do
+  [C.block|void{ std::cout << "Hello, World!\n"; }|]
+  [C.block|void{ foo(); }|]
